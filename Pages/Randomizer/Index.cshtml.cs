cbc608104bcf8d363f336d4109a347189c7e6bd7
Hogan Karr <hskarr0720@gmail.com>
2022-05-04 16:15:35 -0500

    Final Project Done

--- Pages/Randomizer/Index.cshtml.cs
+++ Pages/Randomizer/Index.cshtml.cs
@@ -47,5 +47,27 @@ namespace final_project.Pages.Randomizer
             Heavy = await heavy.Skip(hevSkip).Take(1).ToListAsync();
         }
 
+         public async Task OnPostAsync()
+        {
+            var armor = _context.Armor.Select(a => a);
+            Random Arand = new Random();
+            int armSkip = Arand.Next(1, armor.Count());
+            Armor = await armor.Skip(armSkip).Take(1).ToListAsync();
+
+            var primary = _context.Primary.Select(a => a);
+            Random Prand = new Random();
+            int priSkip = Prand.Next(1, primary.Count());
+            Primary = await primary.Skip(priSkip).Take(1).ToListAsync();
+
+            var secondary = _context.Secondary.Select(a => a);
+            Random Srand = new Random();
+            int secSkip = Srand.Next(1, secondary.Count());
+            Secondary = await secondary.Skip(secSkip).Take(1).ToListAsync();
+
+            var heavy = _context.Heavy.Select(a => a);
+            Random Hrand = new Random();
+            int hevSkip = Hrand.Next(1, heavy.Count());
+            Heavy = await heavy.Skip(hevSkip).Take(1).ToListAsync();
+        }
     }
 }
